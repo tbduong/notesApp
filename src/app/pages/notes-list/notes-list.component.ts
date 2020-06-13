bd03a804563f0183cbc53e27de7c2d330aa1b48e
tbduong <tiffaniduong@gmail.com>
2020-06-12 21:12:10 -0700

    first commit - create routing for main page and import bootstrap

---
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html',
+  styles: [
+  ]
+})
+export class NotesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
